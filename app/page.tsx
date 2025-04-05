ff9eaf3827755ef48600bf6e71343bff75c1d21b
Devp-sriram <sriramraman100@gmail.com>
2025-04-05 18:54:59 +0530

    comopents added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Navbar from './components/Navbar';
+
+
 export default function Home() {
-  return (
-  <div className="">I phone</div>
-  );
+
+    return (
+      <>
+        <Navbar/>
+        <Hero/>
+        <Highlights/>
+      </>
+    );
 }
